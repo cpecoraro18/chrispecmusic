2055e629700e49f4c19db599160e8e45b1d26358
Christopher Pecoraro <cpecoraro18@gmail.com>
2024-07-03 12:58:59 -0500

    Fix email links

--- frontend/src/components/SiteFooter.vue
+++ frontend/src/components/SiteFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer bg-dark text-white text-center py-3 mt-auto">
     <div class="container d-flex justify-content-between">
       <p class="mb-0">&copy; 2024 ChrisPecMusic</p>
-      <p class="mb-0">Email: <a href="mailto:contact@chrispecmusic.com">contact@chrispecmusic.com</a></p>    </div>
+      <p class="mb-0"><a href="mailto:contact@chrispecmusic.com">contact@chrispecmusic.com</a></p>    </div>
   </footer>
 </template>
 

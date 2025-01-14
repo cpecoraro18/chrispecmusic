8be4ac389d5c37b6446bff4bca0c7e2bcb8ffb7a
Christopher Pecoraro <cpecoraro18@gmail.com>
2025-01-14 11:04:09 -0600

    Fix build dir github pages issue

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
   app: {
+    buildAssetsDir: 'assets',
     head: {
       title: 'Chris Pecoraro - Chicago Bass Player',
       meta: [

6b3a6070d50229743502b29a3706d9f64f118042
Christopher Pecoraro <cpecoraro18@gmail.com>
2025-01-18 14:55:12 -0600

    Fix google tag manager

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -29,7 +29,21 @@ export default defineNuxtConfig({
       ],
       script: [
         { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" },
-        { src: "https://kit.fontawesome.com/d05f769d21.js", crossorigin: "anonymous" }
+        { src: "https://kit.fontawesome.com/d05f769d21.js", crossorigin: "anonymous" },
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MFS3DSBM');`,
+          type: 'text/javascript',
+        },
+      ],
+      noscript: [
+        {
+          children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFS3DSBM"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        },
       ]
     }
   },

4b77ec5414cb8ff2348493ba9170a0608ace3b04
Chris Pecoraro <cpecoraro@rumbleup.com>
2026-01-18 10:10:53 -0600

    Better sizing for social links on mobile

--- frontend/components/SocialLinks.vue
+++ frontend/components/SocialLinks.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
-        <a href="mailto:contact@chrispecmusic.com" target="_blank" rel="noopener" class="text-white mx-2" aria-label="Instagram">
-            <i class="fa fa-envelope"></i>
+        <a href="mailto:contact@chrispecmusic.com" target="_blank" rel="noopener" class="text-white mx-2" aria-label="Email">
+            <i class="fa fa-envelope social-icon"></i>
         </a>
         <a href="https://www.instagram.com/chrispecmusic" target="_blank" rel="noopener" class="text-white mx-2" aria-label="Instagram">
-            <i class="fab fa-instagram"></i>
+            <i class="fab fa-instagram social-icon"></i>
         </a>
         <a href="https://www.youtube.com/@ChrisPecMusicOfficial" target="_blank" rel="noopener" class="text-white mx-2" aria-label="YouTube">
-            <i class="fab fa-youtube"></i>
+            <i class="fab fa-youtube social-icon"></i>
         </a>
     </div>
 </template>
+
+<style scoped>
+@media (max-width: 576px) {
+  .social-icon {
+    font-size: 2em;
+  }
+}
+</style>
